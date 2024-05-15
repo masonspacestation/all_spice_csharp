d8d0de9402b5a6dcf77cc26d7ea5fca603257efc
masonspacestation <71409121+masonspacestation@users.noreply.github.com>
2024-05-15 15:30:46 -0600

    created recipes table

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -1,8 +1,29 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
+-- Active: 1715633466013@@127.0.0.1@3306@cooperative_mountain_fe2c50_db
+CREATE TABLE IF NOT EXISTS accounts (
+    id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+    name varchar(255) COMMENT 'User Name',
+    email varchar(255) COMMENT 'User Email',
+    picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8mb4 COMMENT '';
+
+CREATE TABLE recipes (
+    id INT NOT NULL AUTO_INCREMENT primary key,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+    title VARCHAR(255) NOT NULL,
+    instructions VARCHAR(5000) NOT NULL,
+    img VARCHAR(1000) NOT NULL,
+    category ENUM(
+        "breakfast",
+        "lunch",
+        "dinner",
+        "snack",
+        "dessert"
+    ) DEFAULT "dinner",
+    creatorId VARCHAR(255) NOT NULL,
+    FOREIGN KEY (creatorId) REFERENCES accounts (id) ON DELETE CASCADE
+);
+
+DROP TABLE recipes
