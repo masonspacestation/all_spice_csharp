c572e83f46108bba6a07e350337aed0c120d094d
masonspacestation <71409121+masonspacestation@users.noreply.github.com>
2024-05-16 14:03:05 -0600

    setting active recipe in AppState

--- client/src/AppState.js
+++ client/src/AppState.js
@@ -1,9 +1,22 @@
 import { reactive } from 'vue'
+import { Recipe } from "./models/Recipe.js"
+import { Ingredient } from "./models/Ingredient.js"
 
 // NOTE AppState is a reactive object to contain app level data
 export const AppState = reactive({
   /**@type {import('@bcwdev/auth0provider-client').Identity} */
   identity: null,
   /** @type {import('./models/Account.js').Account} user info from the database*/
-  account: null
+  account: null,
+
+/**@type {Recipe[]} */
+recipes: [],
+
+/**@type {Recipe} */
+activeRecipe: null,
+
+/**@type {Ingredient[]} */
+ingredients: []
+
+
 })
--- client/src/services/RecipesService.js
+++ client/src/services/RecipesService.js
@@ -1,3 +1,5 @@
+import { AppState } from "../AppState.js";
+import { Recipe } from "../models/Recipe.js";
 import { api } from "./AxiosService.js"
 
 
@@ -6,12 +8,15 @@ import { api } from "./AxiosService.js"
 class RecipesService{
   async getRecipeById(recipeId) {
     const response = await api.get(`api/recipes/${recipeId}`)
-    console.log(`found recipe with id: ${recipeId}`);
+    console.log(`found recipe with id: ${recipeId}`, response.data);
+    const activeRecipe = new Recipe(response.data)
+    AppState.activeRecipe = activeRecipe
+    console.log("Active Recipe in AppState:", AppState.activeRecipe);
   }
 
   async getAllRecipes(){
   const response = await api.get('api/recipes')
-  console.log('retrieved all recipes 😋');
+  console.log('retrieved all recipes 😋', response.data);
 }
 }
 
