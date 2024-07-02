63226aa3c7e6eb5e651411196c9f3b242c40a7f7
masonspacestation <71409121+masonspacestation@users.noreply.github.com>
2024-07-02 12:16:14 -0600

    added favicon and created reusable footer links component

--- client/src/App.vue
+++ client/src/App.vue
@@ -14,10 +14,22 @@ import Navbar from './components/Navbar.vue'
   </main>
   <footer class="bg-dark">
     <div class="container justify-content-center">
-      <p class="text-success mt-4">
-        The spice must flow 🧿
-      </p>
+
     </div>
+    <FooterLinks />
+    <!-- <div class="row w-75 justify-content-between my-2 mx-auto">
+      <div class="col-12 col-md-4">
+        <span class="text-light opacity-50 fs-6">Site by </span>
+        <a href="https://andrewmason.dev" class="w-auto"><img class="icon"
+            src="../src/assets/img/AMDev-favicon-colorbg.png" title="Andrew Mason - Development and Design" /><span
+            class="link-underline-light link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover">
+            Andrew
+            Mason - Development and Design</span></a>
+      </div>
+      <div class="col-12 col-md-8 text-end">
+        <LinkIcons class="w-auto me-auto" />
+      </div>
+    </div> -->
   </footer>
 </template>
 
@@ -36,5 +48,12 @@ main {
 
 footer {
   min-height: 48px;
+}
+
+.icon {
+  height: 32px;
+  width: 32px;
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ client/src/components/FooterLinks.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+
+<template>
+  <div class="row w-75 justify-content-between my-2 mx-auto">
+    <div class="col-12 col-md-4">
+      <span class="text-light opacity-50 fs-6">Site by </span>
+      <a href="https://andrewmason.dev" class="w-auto"><img class="icon" src="../assets/img/AMDev-favicon-colorbg.png"
+          title="Andrew Mason - Development and Design" /><span
+          class="link-underline-light link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover">
+          Andrew
+          Mason - Development and Design</span></a>
+    </div>
+    <div class="col-12 col-md-8 text-end">
+      <LinkIcons class="w-auto me-auto" />
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped></style>
