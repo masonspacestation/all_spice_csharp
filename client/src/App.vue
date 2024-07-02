0ea66a60a28192b63613a67bed432a4f504d8eee
masonspacestation <71409121+masonspacestation@users.noreply.github.com>
2024-07-02 14:35:01 -0600

    edits to footer links component

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,29 +7,14 @@ import Navbar from './components/Navbar.vue'
 
 <template>
   <header class="container">
-    <!-- <Navbar /> -->
   </header>
   <main class="pb-4">
     <router-view />
   </main>
   <footer class="bg-dark">
     <div class="container justify-content-center">
-
+      <FooterLinks />
     </div>
-    <FooterLinks />
-    <!-- <div class="row w-75 justify-content-between my-2 mx-auto">
-      <div class="col-12 col-md-4">
-        <span class="text-light opacity-50 fs-6">Site by </span>
-        <a href="https://andrewmason.dev" class="w-auto"><img class="icon"
-            src="../src/assets/img/AMDev-favicon-colorbg.png" title="Andrew Mason - Development and Design" /><span
-            class="link-underline-light link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover">
-            Andrew
-            Mason - Development and Design</span></a>
-      </div>
-      <div class="col-12 col-md-8 text-end">
-        <LinkIcons class="w-auto me-auto" />
-      </div>
-    </div> -->
   </footer>
 </template>
 
@@ -43,7 +28,6 @@ import Navbar from './components/Navbar.vue'
 main {
   background-color: #2c3a47;
   background-image: url("https://www.transparenttextures.com/patterns/black-felt.png");
-  /* This is mostly intended for prototyping; please download the pattern and re-host for production environments. Thank you! */
 }
 
 footer {
--- client/src/components/FooterLinks.vue
+++ client/src/components/FooterLinks.vue
@@ -4,16 +4,17 @@
 
 
 <template>
-  <div class="row w-75 justify-content-between my-2 mx-auto">
-    <div class="col-12 col-md-4">
-      <span class="text-light opacity-50 fs-6">Site by </span>
-      <a href="https://andrewmason.dev" class="w-auto"><img class="icon" src="../assets/img/AMDev-favicon-colorbg.png"
-          title="Andrew Mason - Development and Design" /><span
-          class="link-underline-light link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover">
-          Andrew
-          Mason - Development and Design</span></a>
+  <div class="row justify-content-between my-2 mx-auto">
+    <div class="col-12 col-md-8 me-auto">
+      <div class="row justify-content-center justify-content-md-start">
+        <span class="text-light w-auto opacity-50 fs-6">Site by</span>
+        <a href="https://andrewmason.dev" class="w-auto ps-0 ms-0"><img src="../assets/img/AMDev-favicon-colorbg.png"
+            title="Andrew Mason - Development and Design" /><span
+            class="link-underline-light link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover ms-3">Andrew
+            Mason - Development and Design</span></a>
+      </div>
     </div>
-    <div class="col-12 col-md-8 text-end">
+    <div class="col-12 col-md-4 text-center text-md-end">
       <LinkIcons class="w-auto me-auto" />
     </div>
   </div>
